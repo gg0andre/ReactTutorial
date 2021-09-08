ad76bd8fa4458cde881b228c2cac0609bff427f4
Andre Tran <andreviet2009@hotmail.com>
2021-09-08 16:23:27 -0400

    section 11 done

--- 07-Building-a-Food-Order-App/src/components/Layout/HeaderCartButton.js
+++ 07-Building-a-Food-Order-App/src/components/Layout/HeaderCartButton.js
@@ -1,25 +1,47 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-import CartIcon from '../Cart/CartIcon';
+import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = props => {
-    const cartCtx = useContext(CartContext);
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-    }, 0);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return <button className={classes.button} onClick={props.onClick} >
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            {numberOfCartItems}
-        </span>
+  const {items} = cartCtx;
+
+  const btnclasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer);
+    };  
+  }, [items]);
+
+  return (
+    <button className={btnclasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
-export default HeaderCartButton
+export default HeaderCartButton;
--- 07-Building-a-Food-Order-App/src/store/CartProvider.js
+++ 07-Building-a-Food-Order-App/src/store/CartProvider.js
@@ -9,13 +9,53 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
-    const newTotalAmount =
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const exisitngCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (exisitngCartItem) {
+      const updatedItem = {
+        ...exisitngCartItem,
+        amount: exisitngCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-      items: updateItems,
-      totalAmount: newTotalAmount,
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
+  }
+  if (action.type === " REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if(existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id );
+    } else {
+      const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+      updatedItems = [state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+
+    }
+
+    return {
+      items : updatedItems,
+      totalAmount: updatedTotalAmount
+    }
   }
   return defaultCartState;
 };
@@ -30,15 +70,15 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
 
-  const removeItemFromcArtHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", item: id });
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
-    addItems: addItemToCartHandler,
-    removeItems: removeItemFromcArtHandler,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
 
   return (
