bf3ce0c22b5b2b8a03cc5350c17ab5f783e90fde
Andre Tran <andreviet2009@hotmail.com>
2021-09-09 11:50:27 -0400

    section 12 done

--- 08-ALookBehingTheScenesOfReact/src/App.js
+++ 08-ALookBehingTheScenesOfReact/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import './App.css';
+import Buttton from "./components/UI/Button/Button";
+
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Buttton onClick={allowToggleHandler}>Allow Toggling</Buttton>
+      <Buttton onClick={toggleParagraphHandler}>Toggle Paragraph</Buttton>
     </div>
   );
 }
