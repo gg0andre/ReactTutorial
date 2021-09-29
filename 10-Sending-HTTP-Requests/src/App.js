c961f5ec477f29ee46736480e2ab4859ff78089e
Andre Tran <andreviet2009@hotmail.com>
2021-09-28 21:44:08 -0400

    Update App.js

--- 10-Sending-HTTP-Requests/src/App.js
+++ 10-Sending-HTTP-Requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,11 @@ function App() {
   const [isLoading, setIslLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  useEffect(()=>{
+      fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIslLoading(true);
     setError(null);
     try {
@@ -32,7 +36,7 @@ function App() {
       setError(error.message);
     }
     setIslLoading(false);
-  }
+  }, []);
 
   let content = <p>Found no movies.</p>;
 
