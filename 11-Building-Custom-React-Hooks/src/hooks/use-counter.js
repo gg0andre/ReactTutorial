22c0cf55f2e95453ac956985c21c60a3e79e09ac
Andre Tran <andreviet2009@hotmail.com>
2021-10-04 21:27:24 -0400

    section 15.189

--- 11-Building-Custom-React-Hooks/src/hooks/use-counter.js
+++ 11-Building-Custom-React-Hooks/src/hooks/use-counter.js
@@ -1,15 +1,21 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-const useCounter = () =>{
-    const [counter, setCounter] = useState(0);
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
         setCounter((prevCounter) => prevCounter + 1);
-      }, 1000);
-  
-      return () => clearInterval(interval);
-    }, []);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
 };
 
 export default useCounter;
