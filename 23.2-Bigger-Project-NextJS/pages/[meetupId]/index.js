dcfde111631cb632b9e77a74903047bfb57f1c50
Andre Tran <andreviet2009@hotmail.com>
2022-03-12 15:43:19 -0500

    344

--- 23.2-Bigger-Project-NextJS/pages/[meetupId]/index.js
+++ 23.2-Bigger-Project-NextJS/pages/[meetupId]/index.js
@@ -1,13 +1,12 @@
-import { Fragment } from "react/cjs/react.production.min";
-import MeetupDetail from "../../components/meetups/MeetupDetail";
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails() {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Some street 5, some City"
-      description="This is a first meetup"
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a first meetup'
     />
   );
 }
@@ -18,33 +17,37 @@ export async function getStaticPaths() {
     paths: [
       {
         params: {
-          meetupId: "m1",
+          meetupId: 'm1',
         },
       },
       {
         params: {
-          meetupId: 'm2'
-        }
-      }
+          meetupId: 'm2',
+        },
+      },
     ],
   };
 }
+
 export async function getStaticProps(context) {
   // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
+  console.log(meetupId);
+
   return {
     props: {
       meetupData: {
         image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
         id: meetupId,
-        title: "First Meetup",
-        address: "Some street 5, some City",
-        description: "This is a first meetup",
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
       },
     },
   };
 }
+
 export default MeetupDetails;
--- 23.2-Bigger-Project-NextJS/pages/api/new-meetup.js
+++ 23.2-Bigger-Project-NextJS/pages/api/new-meetup.js
@@ -1,13 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
 
-    
+    const client = await MongoClient.connect(
+      "mongodb+srv://andre1:Testing.123@cluster0.deabv.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
